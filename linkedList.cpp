07320345080eabb6b506853dd67f03a8a2db0286
PRASANJIT ROY <roy2402011@gmail.com>
2026-03-19 23:27:15 +0600

    by me

---
+++ linkedList.cpp
@@ -0,0 +1,54 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+// class Node
+// {   public :
+//     int val ;
+//     Node* next ;
+   
+// };
+
+// int main(){
+//      Node a,b,c;
+//      a.val=10;
+//      b.val=20;
+//      c.val=30;
+//      a.next=&b;
+//      b.next=&c;
+//      c.next=NULL;
+
+//      cout<<a.val<<" "<<a.next->val<<" "<<a.next->next->val<<endl;
+
+
+//     return 0;
+// }
+
+// Dynamic Node
+// #include <bits/stdc++.h>
+// using namespace std;
+// class Node
+// {
+// public:
+//     int val;
+//     Node* next;
+
+//     Node(int val)
+//     {
+//         this->val = val;
+//         this->next = NULL;
+//     }
+// };
+
+// int main() {
+//      Node * head =new  Node(20);
+//      Node * a = new Node(30);
+//      Node * b = new Node(40);
+//      head->next=a;
+//      a->next=b;
+
+//      cout<<head->val<<endl;
+//      cout<<head->next->val<<endl;
+//      cout<<head->next->next->val<<endl;
+     
+
+//     return 0;
+// }
