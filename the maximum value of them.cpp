626bb8c4bc6b098463ff9178c3760d80d60b24bd
PRASANJIT ROY <roy2402011@gmail.com>
2026-03-20 11:32:56 +0600

    by me

---
+++ the maximum value of them.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node* next;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insertion(Node* &head,Node* &tail, int val)
+{   Node* newnode = new Node(val);
+    if(head==NULL)
+    {
+        head = newnode;
+        tail = newnode;
+    }
+    else 
+    {
+        tail->next = newnode;
+        tail=newnode;
+    }
+}
+// void reverse(Node* &tmp)
+// {
+//     if(tmp==NULL)
+//     {
+//         return ;
+//     }
+//     reverse(tmp->next);
+//     cout<<tmp->val<<" ";
+// }
+
+// void print_linked_list(Node* head)
+// {   
+//     Node* temp = head;
+//     while(temp != NULL)
+//     {
+//         cout << temp->val <<" ";
+//         temp = temp->next;
+//     }
+// }
+
+int mx(Node* tmp)
+{
+    int M=INT_MIN;
+    while(tmp->next != NULL)
+    {
+        if(tmp->val>M)
+        {
+            M=tmp->val;
+        }
+         tmp=tmp->next;
+    }
+    return M;
+}
+
+int main() {
+     Node* head = NULL;
+     Node* tail = NULL;
+     int val;
+     while(true)
+     {
+        cin>>val;
+        if(val== -1)
+        {
+            break;
+        }
+        insertion(head,tail,val);
+     }
+
+    // print_linked_list(head);
+    Node* tmp = head;
+    cout<<mx(tmp)<<endl;
+    return 0;
+}
